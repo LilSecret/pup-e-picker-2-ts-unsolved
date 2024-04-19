591e7ed3958543fc7b031d1506c260fd00ce6d21
lilsecret <chuckm248@gmail.com>
2024-04-19 11:19:58 -0600

    update api requests

--- src/api.tsx
+++ src/api.tsx
@@ -1,16 +1,45 @@
+import { TDog } from "./types";
+
+const BASE_URL = "http://localhost:3000";
+
+const deleteDogRequest = (id: number) => {
+  return fetch(`${BASE_URL}/dogs/${id}`, {
+    method: "DELETE",
+    body: JSON.stringify({}),
+    headers: {
+      "Content-type": "application/json",
+    },
+  }).then((res) => res.json());
+};
+
+const postDog = (dog: Omit<TDog, "id">) => {
+  return fetch(`${BASE_URL}/dogs`, {
+    method: "POST",
+    body: JSON.stringify(dog),
+    headers: {
+      "Content-type": "application/json",
+    },
+  }).then((res) => res.json());
+};
+
+const patchFavoriteForDog = (dog: Partial<TDog>, id: number) => {
+  return fetch(`${BASE_URL}/dogs/${id}`, {
+    method: "PATCH",
+    body: JSON.stringify(dog),
+    headers: {
+      "Content-type": "application/json",
+    },
+  }).then((res) => res.json());
+};
+
 const getAllDogs = () => {
-  // fill out method
-};
-
-const postDog = () => {
-  // fill out method
-};
-const deleteDogRequest = () => {
-  // fill out method
-};
-
-const patchFavoriteForDog = () => {
-  // fill out method
+  return fetch(`${BASE_URL}/dogs`, {
+    method: "GET",
+    body: JSON.stringify({}),
+    headers: {
+      "Content-type": "application/json",
+    },
+  }).then((res) => res.json());
 };
 
 export const Requests = {
